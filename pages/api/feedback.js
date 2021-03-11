f405b302e13f3d498412c5d68f3e5d733931f71c
Maksym <maksym.sh@upplabs.com>
2021-03-11 17:28:33 +0200

    post-and-get-requests

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
 const handler = (req, res) => {
-  res.status(200).json({ text: 'Next api works' });
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedback = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    };
+
+    // store to database or to a file
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json'); // cwd current working directory
+    const fileData = fs.readFileSync(filePath);
+    const parsedData = JSON.parse(fileData);
+
+    parsedData.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(parsedData));
+    res.status(201).json({ message: 'Update' });
+  } else {
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json'); // cwd current working directory
+    const fileData = fs.readFileSync(filePath);
+    const parsedData = JSON.parse(fileData);
+    res.status(200).json({ data: parsedData });
+  }
 };
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,67 @@
+import { useRef, useState } from 'react';
+
 function HomePage() {
+  const emailRef = useRef(null);
+  const feedbackRef = useRef(null);
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        feedback: feedbackRef.current.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+  };
+
+  const loadFeedback = () => {
+    fetch('/api/feedback', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        debugger;
+        setFeedbacks(res.data);
+      });
+  };
+
+  if (!feedbacks) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={onSubmitHandler}>
+        <div>
+          <label htmlFor='email'>Enter you email</label>
+          <input type='email' id='email' ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Enter you feedback</label>
+          <textarea id='feedback' rows='7' ref={feedbackRef}></textarea>
+        </div>
+        <button>Submit</button>
+      </form>
+      <button onClick={loadFeedback}>Load Feedback</button>
+      <div>
+        {feedbacks.map(({ email, id, feedback }) => (
+          <ul key={id}>
+            <li>{email}</li>
+            <li>{feedback}</li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
